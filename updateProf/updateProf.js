37606fdc5bb63dea4fa22c600e9261c1785d38fe
Ryan Bryce <ryanbryce@Ryans-MacBook-Pro.local>
2017-02-20 22:56:09 -0700

    update profile

--- updateProf/updateProf.js
+++ updateProf/updateProf.js
@@ -1,3 +1,18 @@
-angular.module('app').controller('updateCtrl', function($scope){
+angular.module('app').controller('updateCtrl', function($scope, friendService){
+  $scope.setData = function(name, tagline, profileUrl, bio){
+    friendService.data.name = name;
+    friendService.data.tagline = tagline;
+    friendService.data.profileUrl = profileUrl;
+    friendService.data.bio = bio;
+  };
   
+  $scope.person = friendService.person();
+  console.log($scope.person);
+
+  $scope.splitter = function(){
+  var nameSplit = $scope.person.name.split(' ');
+  $scope.firstName = nameSplit[0];
+  $scope.lastName = nameSplit[nameSplit.length - 1];
+  }
+  $scope.splitter();
 });
